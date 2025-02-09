6273e508fbe8a3947340047c823f0171ca0f6376
RafAraujoDev <rafael.araujodev5@gmail.com>
2025-02-09 18:37:50 -0300

    game_option

--- scripts.js
+++ scripts.js
@@ -5,13 +5,25 @@ const machineScore = document.querySelector("#machine-score")
 let humanScoreNumber = 0
 let machineScoreNumber = 0
 
+/*
+humanScoreNumber -> Camel Case
+GAME_OPTIONS     -> Snake Case
+*/
+
+// Enum criar um dicionário de opções 
+const GAME_OPTIONS = {
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors'
+}
+
 const playHuman = (humanChoice) => {
 
     playTheGame(humanChoice, playMachine())
 }
 
 const playMachine = () => {
-    const choices = ["rock", "paper", "scissors"]
+    const choices = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
     const randomNumber = Math.floor(Math.random() * 3)
 
     return choices[randomNumber]
@@ -24,9 +36,9 @@ const playTheGame = (human, machine) => {
     if (human === machine) {
         result.innerHTML = "Deu empate"
     } else if (
-        (human === 'paper' && machine === 'rock') ||
-        (human === 'rock' && machine === 'scissors') ||
-        (human === 'scissors' && machine === 'paper')
+        (human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK) ||
+        (human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS) ||
+        (human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER)
     ) {
         humanScoreNumber++
         humanScore.innerHTML = humanScoreNumber
